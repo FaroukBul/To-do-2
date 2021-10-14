2cdc638ba10ee71de9d342f58c723f1a4f2c6fc0
Farouk <farouk_jr@hotmail.com>
2021-10-14 17:21:56 -0500

    Removed date input and assigned ids to cards

--- app.js
+++ app.js
@@ -1,35 +1,44 @@
+let ids = []
 document.addEventListener("DOMContentLoaded", () =>{
     let addBtn = document.getElementById("add-btn")
     addBtn.addEventListener("click", ()=>{
         let newTodo = new Todo
         newTodo.createCard()
     })
+
 })
 
 class Todo {
 
     constructor(){
+        this.ids = ids
         this.container = document.getElementsByClassName("cards-container")[0]
         this.newCard = null
         this.textContainer = null
-        this.dateContainer = null
         this.btnContainer = null
         this.text = document.getElementById("text").value
-        this.date = document.getElementById("set-date").value.toString()
     }
 
     createCard(){
         this.newCard = this.createDiv()
         this.textContainer = this.createDiv()
-        this.dateContainer = this.createDiv()
         this.textContainer.innerHTML = this.text
-        this.dateContainer.innerHTML = this.date
+        this.newCard.classList.add("card")
+        this.newCard.setAttribute("id", this.generateId())
         this.newCard.appendChild(this.textContainer)
-        this.newCard.appendChild(this.dateContainer)
         this.container.appendChild(this.newCard)
     }
 
     createDiv(){
         return document.createElement("div")
+    }
+
+    generateId(){
+        let id = 0
+        if (this.ids.length > 0){
+            id = this.ids[this.ids.length - 1] + 1
+        }
+        this.ids.push(id)
+        return id
     }
 }
